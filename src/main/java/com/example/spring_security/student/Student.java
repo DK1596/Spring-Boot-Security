8250c7e92439d9ef1468554543b22d64e7696ce4
DK1596 <dinas_1996@mail.ru>
2021-07-02 15:09:36 +0600

    simple rest controller with one object

---
+++ src/main/java/com/example/spring_security/student/Student.java
@@ -0,0 +1,19 @@
+package com.example.spring_security.student;
+
+public class Student {
+    private final Integer id;
+    private final String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
